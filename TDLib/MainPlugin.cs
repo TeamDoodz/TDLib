8a017c791b1895fe3350f633b52a7c970bfd0261
TeamDoodz <unconfigured@null.spigotmc.org>
2022-03-18 15:07:55 -0500

    stuff

--- TDLib/MainPlugin.cs
+++ TDLib/MainPlugin.cs
@@ -21,7 +21,7 @@ namespace TDLib {
 
 		internal const string GUID = "io.github.TeamDoodz." + Name;
 		internal const string Name = "TDLib";
-		internal const string Version = "2.0.0";
+		internal const string Version = "1.2.0";
 
 		/// <summary>
 		/// Whether or not this mod has been loaded yet. If you want to do stuff after the mod has been loaded it is recommended to instead make a <see cref="BepInDependency"/> to the mod.
@@ -30,8 +30,6 @@ namespace TDLib {
 
 		internal static ManualLogSource logger;
 		internal static ConfigFile cfg;
-
-		bool PrefixInsensitiveExists = false;
 
 		private void Awake() {
 			logger = Logger;
